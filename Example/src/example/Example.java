adc6bd19d2cd7689457e667fc51d005fc7ee41e5
Cryaton McIntosh <craytontosh@gmail.com>
2016-09-01 10:24:08 -0500

    Java Practice
    I am currently going through “Java: A Beginner’s Guide” by Herbert
    Schildt and this is where I will be posting my chapter practice problems

---
+++ Example/src/example/Example.java
@@ -0,0 +1,11 @@
+/*
+Author: Crayton McIntosh
+    This is a simple Java program
+    My first  Java project
+ */
+package example;
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Java drives the web.");
+    } 
+}
